0dcef36e5d9201cb1c3f287db61c11e490fb3ee5
gdi <xxx@yy.com>
2016-02-28 22:19:44 +0800

    add msodbcsql install

--- installodbc.sh
+++ installodbc.sh
@@ -1,10 +1,15 @@
-tar -xvzf msodbcsql-13.0.0.0.tar.gz
-tar -xvzf unixodbc-2.3.1.tar.gz
-echo "123" | sudo -S apt remove unixodbc
+#tar -xvzf msodbcsql-13.0.0.0.tar.gz
+#tar -xvzf unixodbc-2.3.1.tar.gz
+#echo "123" | sudo -S apt remove unixodbc
 cd unixODBC-2.3.1
 CPPFLAGS="-DSIZEOF_LONG_INT=8"
 export CPPFLAGS
-./configure --prefix=/usr --libdir=/usr/lib64 --sysconfdir=/etc --enable-gui=no --enable-drivers=no --enable-iconv --with-iconv-char-enc=UTF8 --with-iconv-ucode-enc=UTF16LE
-echo "123" | sudo -S make
-echo "123" | sudo  -S make install
+#./configure --prefix=/usr --libdir=/usr/lib64 --sysconfdir=/etc --enable-gui=no --enable-drivers=no --enable-iconv --with-iconv-char-enc=UTF8 --with-iconv-ucode-enc=UTF16LE
+#echo "123" | sudo -S make
+#echo "123" | sudo  -S make install
 
+cd ../msodbcsql-13.0.0.0
+echo "123" | sudo -S apt-get install libgss3
+echo "123" | sudo -S ./install.sh verify
+echo "YES" | echo "Q" | echo "123" | sudo -S ./install.sh install 
+#odbcinst -q -d -n  "ODBC Driver 13 for SQL Server"
