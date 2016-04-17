6a6f333e64339923d92014e55cbf197c07f3f419
gdi <xxx@yy.com>
2016-04-17 20:56:52 +0800

    some dependencies libs for building python

---
+++ deps_compile_python.sh
@@ -0,0 +1,11 @@
+#pyenv编译python可能需要的依赖库
+echo "123" | sudo -S apt-get -y install libc6-dev
+echo "123" | sudo -S apt-get -y install build-essential 
+echo "123" | sudo -S apt-get -y install libssl-dev
+echo "123" | sudo -S apt-get -y install zlib1g-dev
+echo "123" | sudo -S apt-get -y install libbz2-dev
+echo "123" | sudo -S apt-get -y install libreadline-dev
+echo "123" | sudo -S apt-get -y install libsqlite3-dev
+echo "123" | sudo -S apt-get -y install wget 
+echo "123" | sudo -S apt-get -y install curl
+echo "123" | sudo -S apt-get -y install llvm
