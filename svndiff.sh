1605fce63800fe2353d25da083bb93394ac1081e
gdi <xxx@yy.com>
2016-09-11 14:03:31 +0800

    add description

--- svndiff.sh
+++ svndiff.sh
@@ -4,3 +4,10 @@ LEFT=${6}
 RIGHT=${7}
 $DIFF $LEFT $RIGHT 
 
+
+
+#Usage: 
+#   1, put this file in /usr/local/bin
+#   2, modify ./subversion/config, write a line 
+#        "diff-cm=/usr/local/bin/svndiff.sh" 
+#   3, now, you can vimdiff as svn diff tool
