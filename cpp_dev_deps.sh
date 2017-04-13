f14d6ac662363f1828e430f25c6aca4b427f23ff
gdi <xxx@yy.com>
2017-04-13 21:30:03 +0800

    add more details

--- cpp_dev_deps.sh
+++ cpp_dev_deps.sh
@@ -1,1 +1,6 @@
 # Download c/cpp develop dependencies and tool chains
+echo "123" | sudo -S apt-get -y install build-essential
+echo "123" | sudo -S apt-get -y install ccache
+echo "123" | sudo -S apt-get -y install cscope
+echo "123" | sudo -S apt-get -y install clang
+echo "123" | sudo -S apt-get -y install cmake
--- installed_necessary.sh
+++ installed_necessary.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 echo "123" | sudo -S apt-get -y install mysql-server-5.7 mysql-client-5.7
+echo "123" | sudo -S apt-get -y install fcitx fcitx-table-wubi fcitx-table-wubi-large fcitx-sunpinyin 
+
+echo "123" | sudo -S apt-get -y install vim
+echo "123" | sudo -S apt-get -y install exuberant-ctags
+echo "123" | sudo -S apt-get -y install lrzsz 
+echo "123" | sudo -S apt-get -y install autoconf
+echo "123" | sudo -S apt-get -y install autojump
+echo "123" | sudo -S apt-get -y install openssh-server
+echo "123" | sudo -S apt-get -y install terminator
+
+
 
