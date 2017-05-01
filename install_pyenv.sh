01170f5c32649e49fe1e2408d6c84915075470b8
dilworm <dilworm@live.com>
2017-05-01 10:03:44 +0800

    comment bashrc statements

--- install_pyenv.sh
+++ install_pyenv.sh
@@ -2,7 +2,7 @@
 # github.com/yyuu/pyenv
 # Change bashrc to your own shell option file
 git clone https://github.com/yyuu/pyenv.git ~/.pyenv
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(pyenv init -)"' >> ~/.bashrc  # make sure this write to the end of shell configurea file
-exec $SHELL
+#echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+#echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+#echo 'eval "$(pyenv init -)"' >> ~/.bashrc  # make sure this write to the end of shell configurea file
+#exec $SHELL
