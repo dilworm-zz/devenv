e52198cbe74346f978452a6c677778b386092735
gdi <xxx@yy.com>
2017-08-09 14:53:15 +0800

    add python-dev

--- install_tools.sh
+++ install_tools.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 pwd=123
-echo $pwd | sudo -S apt-get -y install mysql-server-5.7 mysql-client-5.7
+echo $pwd | sudo -S apt install python-dev
+echo $pwd | sudo -S apt-get -y install mysql-server-5.7 mysql-client-5.7 libmysqld-dev
 echo $pwd | sudo -S apt-get -y install fcitx fcitx-table-wubi fcitx-table-wubi-large fcitx-sunpinyin 
 
 echo $pwd | sudo -S apt-get -y install vim vim-gnome
