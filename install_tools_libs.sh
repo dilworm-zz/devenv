d09c1ac6787d5e77b68fe0000348431d5d1deb35
gdi <xxx@yy.com>
2018-04-23 00:13:33 +0800

    add ack-grep

--- install_tools_libs.sh
+++ install_tools_libs.sh
@@ -2,7 +2,7 @@
 pwd=123
 echo $pwd | sudo -S apt install intltool itstool
 echo $pwd | sudo -S apt install python-dev 
-echo $pwd | sudo -S apt-get -y install mysql-server-5.7 mysql-client-5.7 libmysqld-dev mysql-workbench
+#echo $pwd | sudo -S apt-get -y install mysql-server-5.7 mysql-client-5.7 libmysqld-dev mysql-workbench
 echo $pwd | sudo -S apt-get -y install fcitx fcitx-table-wubi fcitx-table-wubi-large fcitx-sunpinyin 
 
 echo $pwd | sudo -S apt-get -y install vim vim-gnome
@@ -12,4 +12,4 @@ echo $pwd | sudo -S apt-get -y install autoconf
 echo $pwd | sudo -S apt-get -y install autojump
 echo $pwd | sudo -S apt-get -y install openssh-server
 echo $pwd | sudo -S apt-get -y install terminator
-
+echo $pwd | sudo -S apt-get -y install ack-grep
